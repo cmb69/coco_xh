133334432c99d7af401064f61821d2bd1f8abe3d
cmb69 <cmbecker69@gmx.de>
2012-03-31 21:09:31 +0000

    added German translation

---
+++ languages/de.php
@@ -0,0 +1,20 @@
+<?php
+
+$plugin_tx['coco']['syscheck_title']="System-Prüfung";
+$plugin_tx['coco']['syscheck_phpversion']="PHP-Version ≥ %s";
+$plugin_tx['coco']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['coco']['syscheck_encoding']="UTF-8-Kodierung eingestellt";
+$plugin_tx['coco']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['coco']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+$plugin_tx['coco']['title_cocos']="Vorhandene Co-Contents";
+
+$plugin_tx['coco']['menu_main']="Co-Contents";
+
+$plugin_tx['coco']['confirm_delete']="Der gesamte Inhalt von \"%s\" wird gelöscht!";
+
+$plugin_tx['coco']['error_invalid_name']="Co-Content-Namen dürfen nur a-z, 0-9 and _ enthalten!";
+
+$plugin_tx['coco']['cf_folder_data']="Pfad zu einem Ordner relativ zum CMSimple Wurzelverzeichnis, in dem die Plugin-Daten gespeichert werden. Z.B. \"userfiles/coco/\". Leer lassen, um in den data/ Ordner des Plugins zu speichern.";
+
+?>
