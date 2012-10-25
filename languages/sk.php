bfde5eb8a4192e896c9bc338bf81e77caf2c88e0
cmb69 <cmbecker69@gmx.de>
2012-10-25 16:20:53 +0000

    updated Slovak translation (contributed by Tata)

--- languages/sk.php
+++ languages/sk.php
@@ -1,21 +1,15 @@
 <?php
 
-/*
- * Copyright (c) 2012 Dr. Martin Sereday - www.cmsimple.sk - info(at)cmsimple.sk
- *
- * This work is licensed under a Creative Commons Attribution 3.0 Unported
- * License.
- */
-
-$plugin_tx['coco']['utf8-marker']="äöüßľščťžýáíéňô";
 $plugin_tx['coco']['syscheck_title']="Kontrola systému";
 $plugin_tx['coco']['syscheck_phpversion']="PHP verzia ≥ %s";
 $plugin_tx['coco']['syscheck_extension']="Rozšírenie '%s' načítané";
 $plugin_tx['coco']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
 $plugin_tx['coco']['syscheck_magic_quotes']="Magic quotes runtime off";
 $plugin_tx['coco']['syscheck_writable']="Do adresára '%s' možno zapisovať";
+$plugin_tx['coco']['title_cocos']="Existujúce Co-Contents";
+$plugin_tx['coco']['menu_main']="Co-Contents";
+$plugin_tx['coco']['confirm_delete']="Celý obsah \"%s\" bude vymazaný!";
 $plugin_tx['coco']['error_invalid_name']="Názvy v Co-content môžu obsahovať iba  a-z, 0-9 a _ !";
-$plugin_tx['coco']['cf_utf8-marker']="Iba pre vnútorné použitie. <strong>Nič nemeňte!</strong>";
 $plugin_tx['coco']['cf_folder_data']="Cesta k adresáru relatívna ku koreňovému adresáru CMSimple, kam sa majú ukladať dáta pluginu. Napr.: \"userfiles/coco/\". Ak nič neuvediete, budú sa dáta zapisovať do adresára pluginu /data.";
 
 ?>
