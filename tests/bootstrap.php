a0b567dcb96cb364b5ab22b81f0cf5d46eb700fe
Christoph M. Becker <cmbecker69@gmx.de>
2023-03-06 11:02:46 +0100

    Tweak test infrastructure
    * Avoid unit test terminology
    *  Autoload plugin classes for tests
    * Move coverage report to top-level folder
    * Drop attack-test (should be done in another way)
    * Update to PHPUnit 9.5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -22,5 +22,19 @@
 require_once "../../cmsimple/functions.php";
 require_once "../../cmsimple/classes/PageDataRouter.php";
 require_once "../../cmsimple/classes/Pages.php";
-require_once "./classes/CocoService.php";
-require_once "./classes/IdGenerator.php";
+
+spl_autoload_register(function (string $className) {
+    $parts = explode("\\", $className);
+    if ($parts[0] !== "Coco") {
+        return;
+    }
+    if (count($parts) === 3) {
+        $parts[1] = strtolower($parts[1]);
+    }
+    $filename = implode("/", array_slice($parts, 1));
+    if (is_readable("./classes/$filename.php")) {
+        include_once "./classes/$filename.php";
+    } elseif (is_readable("./tests/$filename.php")) {
+        include_once "./tests/$filename.php";
+    }
+});
